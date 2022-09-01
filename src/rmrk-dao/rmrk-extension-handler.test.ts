43a09548a4266f6b939e5d771d6deccc107b4fe5
Spencer Brown <git@spencerwebservices.com>
2022-08-31 22:23:42 -0700

    Test processing REGISTER interaction

--- src/rmrk-dao/rmrk-extension-handler.test.ts
+++ src/rmrk-dao/rmrk-extension-handler.test.ts
@@ -5,17 +5,15 @@ import { appendPromise, prefixes } from '../rmrk2/utils'
 import { MemoryDatabaseAdapter } from './database-adapter/memory-database-adapter'
 import { RmrkExtensionHandler } from './rmrk-extension-handler'
 
-describe('Process RMRKDAO remarks', () => {
-  let _rawBlocks: any[]
-  let _remarks: Remark[]
-
-  beforeAll(async () => {
-    const filePath = 'src/__tests__/data/block_with_propose_interaction.json'
-    _rawBlocks = await appendPromise(filePath)
-    _remarks = getRemarksFromBlocks(_rawBlocks, prefixes, KUSAMA_SS58_FORMAT)
-  })
-
+describe('Process RMRKDAO remarks from block data', () => {
   test('successfully parse PROPOSE interaction', async () => {
+    const filePath = 'src/__tests__/data/block_with_propose_interaction.json'
+    const rawBlocks = await appendPromise(filePath)
+    const remarks = getRemarksFromBlocks(
+      rawBlocks,
+      prefixes,
+      KUSAMA_SS58_FORMAT
+    )
     const db = new MemoryDatabaseAdapter()
     db.custodians['J1DxVnsH7Pb2bvvUo771EFv9eAwyjoAasFvLvDyBQCg2XjZ'] = {
       custodian: 'J1DxVnsH7Pb2bvvUo771EFv9eAwyjoAasFvLvDyBQCg2XjZ',
@@ -29,7 +27,7 @@ describe('Process RMRKDAO remarks', () => {
     db.blockTimes[14252786] = 1661983080000
 
     const handler = new RmrkExtensionHandler(db)
-    const remark = _remarks[0]
+    const remark = remarks[0]
 
     await handler.processRemark(remark)
 
@@ -54,6 +52,35 @@ Object {
   "passingThreshold": undefined,
   "snapshot": 1661983083000,
   "startDate": 1661983083000,
+}
+`)
+  })
+
+  test('successfully parse PROPOSE interaction', async () => {
+    const filePath = 'src/__tests__/data/block_with_register_interaction.json'
+    const rawBlocks = await appendPromise(filePath)
+    const remarks = getRemarksFromBlocks(
+      rawBlocks,
+      prefixes,
+      KUSAMA_SS58_FORMAT
+    )
+    const db = new MemoryDatabaseAdapter()
+    const handler = new RmrkExtensionHandler(db)
+    const remark = remarks[0]
+
+    await handler.processRemark(remark)
+
+    // TODO: Consider making sure that fee properties return BigInt from Postgres database
+    expect(db.custodians['DaoQkNgxobgpKfd9NmETnWe6FCfDERduSTR2YiFDRGKvL17'])
+      .toMatchInlineSnapshot(`
+Object {
+  "block": 14255141,
+  "changes": Array [],
+  "custodian": "DaoQkNgxobgpKfd9NmETnWe6FCfDERduSTR2YiFDRGKvL17",
+  "maxOptions": 100,
+  "proposalFee": 110000000000n,
+  "recertifyFee": 1000000000000n,
+  "voteFee": 50000000000n,
 }
 `)
   })
