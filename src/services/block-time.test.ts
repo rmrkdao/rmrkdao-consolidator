c60efd21f51232856775bde206cb1416bef14652
Spencer Brown <git@spencerwebservices.com>
2022-09-01 00:12:06 -0700

    Don't run brittle tests without env variable

--- src/services/block-time.test.ts
+++ src/services/block-time.test.ts
@@ -2,10 +2,15 @@ import { getApiWithReconnect } from 'rmrk-tools'
 import { getBlockTime } from './block-time'
 jest.setTimeout(30000)
 
-// TODO: Brittle test as it depends on the availability of the public Kusama endpoint
-test('can get block time', async () => {
-  const api = await getApiWithReconnect(['wss://kusama-rpc.polkadot.io'])
-  const blockTime = await getBlockTime(api, 8788585)
+// Brittle test as it depends on the availability of the public Kusama endpoint
+if (process.env.RUN_BRITTLE_TESTS === 'true') {
+  test('can get block time', async () => {
+    const api = await getApiWithReconnect(['wss://kusama-rpc.polkadot.io'])
+    const blockTime = await getBlockTime(api, 8788585)
+    await api.disconnect()
 
-  expect(blockTime).toBe(1628967570006)
-})
+    expect(blockTime).toBe(1628967570006)
+  })
+} else {
+  test('empty test', () => {})
+}
